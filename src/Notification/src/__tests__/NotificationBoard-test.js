2acdcd1414898ccfceedba7eff48712ff1799062
zpytchrb <radu.brehar@zippyui.com>
2018-02-27 10:38:30 +0200

    [cmp=Notification] Cleanup

--- src/Notification/src/__tests__/NotificationBoard-test.js
+++ src/Notification/src/__tests__/NotificationBoard-test.js
@@ -1,16 +1,3 @@
-/**
- * Copyright 2015-present Zippy Technologies
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *   http://www.apache.org/licenses/LICENSE-2.0
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 import React from 'react';
 import NotificationBoard from '../NotificationBoard';
 import Notification from '../Notification';
@@ -30,7 +17,12 @@ describe('NotificationBoard', () => {
       }
     });
     expect(wrapper.find(Notification)).to.have.length(1);
-    expect(wrapper.find(Notification).at(0).props().style).to.deep.equal({
+    expect(
+      wrapper
+        .find(Notification)
+        .at(0)
+        .props().style
+    ).to.deep.equal({
       backgroundColor: 'lightBlue',
       background: 'blue',
       color: 'purple'
@@ -77,7 +69,10 @@ describe('NotificationBoard', () => {
 
     wrapper.instance().addNotification();
 
-    const notificationProps = wrapper.find(Notification).at(0).props();
+    const notificationProps = wrapper
+      .find(Notification)
+      .at(0)
+      .props();
     // all common props should be present on notificationProps
     const test = Object.keys(defaultProps).filter(
       propName => notificationProps[propName]
--- src/Notification/src/utils/getPositions/getPositionsRelativeToRegion.js
+++ src/Notification/src/utils/getPositions/getPositionsRelativeToRegion.js
@@ -1,29 +1,10 @@
 /**
- * Copyright 2015-present Zippy Technologies
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *   http://www.apache.org/licenses/LICENSE-2.0
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-/**
  * Takes in account the position of the region
  * and corrects the positions to be relative to it.
     (referenceRegion.height - region.bottom) = css relative bottom
     bacuse region.bottom is the distance from top to bottom
 */
-function getPositionsRelativeToRegion(
-  {
-    region,
-    positions
-  }
-) {
+function getPositionsRelativeToRegion({ region, positions }) {
   return positions.map(position => {
     const newPosition = {};
 
