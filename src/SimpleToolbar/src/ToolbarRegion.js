658499899ac024e17f26d126dd3907904038bdce
zpytchrb <radu.brehar@zippyui.com>
2018-02-27 14:38:02 +0200

    [cmp=SimpleToolbar] Cleanup

--- src/SimpleToolbar/src/ToolbarRegion.js
+++ src/SimpleToolbar/src/ToolbarRegion.js
@@ -1,27 +1,10 @@
-/**
- * Copyright 2015-present Zippy Technologies
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
-
- *   http://www.apache.org/licenses/LICENSE-2.0
-
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 import React, { cloneElement } from 'react';
 import PropTypes from 'prop-types';
-import assign from 'object-assign';
 
-import cleanProps from '@zippytech/react-clean-props';
-import { Item } from '@zippytech/react-flex';
+import { Item } from '../../Flex';
 
-import join from './join';
+import cleanProps from '../../common/cleanProps';
+import join from '../../common/join';
 
 const CLASS_NAME = 'zippy-react-simple-toolbar__region';
 
