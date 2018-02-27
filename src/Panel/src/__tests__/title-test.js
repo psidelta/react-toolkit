bf78eac5f998e84a484e6900c3bf046bc02bc360
zpytchrb <radu.brehar@zippyui.com>
2018-02-27 11:01:27 +0200

    [cmp=Cleanup] Panel

--- src/Panel/src/__tests__/title-test.js
+++ src/Panel/src/__tests__/title-test.js
@@ -1,17 +1,3 @@
-/**
- * Copyright 2015-present Zippy Technologies
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *   http://www.apache.org/licenses/LICENSE-2.0
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 import React from 'react';
 import Panel, { CLASS_NAME } from '../Panel';
 import { mount } from 'enzyme';
@@ -26,9 +12,12 @@ describe('title', () => {
     it('should render as title text when string', () => {
       const title = 'hello world';
       wrapper.setProps({ title });
-      expect(wrapper.find(`.${CLASS_NAME}__title`).first().text()).to.equal(
-        title
-      );
+      expect(
+        wrapper
+          .find(`.${CLASS_NAME}__title`)
+          .first()
+          .text()
+      ).to.equal(title);
     });
 
     it('should render jsx', () => {
