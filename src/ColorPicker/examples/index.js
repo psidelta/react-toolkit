0480481366929024c2ab8d321622a2ba87517a47
zpytchaec <alin.custura@zippytech.io>
2018-03-21 12:34:04 +0200

    [cmp=ColorPicker] Examples (#42)

--- src/ColorPicker/examples/index.js
+++ src/ColorPicker/examples/index.js
@@ -12,44 +12,59 @@
  * limitations under the License.
  */
 
-import '../style/index.scss'
+import '../style/index.scss';
 
-import React from 'react'
-import { render } from 'react-dom'
-import autoBind from '@zippytech/react-class/autoBind'
-import ColorPicker from '../src'
+import React from 'react';
+import { render } from 'react-dom';
+import autoBind from '@zippytech/react-class/autoBind';
+import ColorPicker from '../src';
+import ColorPalette from '../../ColorPalette';
+import ColorPickerInput from '../../ColorPickerInput';
+import ColorTextInput from '../../ColorTextInput';
+
+import '../style/index.scss';
+import '../../ColorPalette/style/index.scss';
+import '../../ColorPickerInput/style/index.scss';
+import '../../ColorTextInput/style/index.scss';
 
 class App extends React.Component {
   constructor(props) {
-    super(props)
-    autoBind(this)
+    super(props);
+    autoBind(this);
     this.state = {
-      color: "#1b3f9c"
-    }
+      color: '#1b3f9c'
+    };
   }
 
   onChange(stringColor, color) {
     this.setState({
-      color,
+      color
       // stringColor
-    })
+    });
   }
 
   render() {
-    return <div style={{ display: 'flex', width: 320 }}>
-      <ColorPicker
-        // value={this.state.color}
-        // onDrag={this.onDrag}
-        // saturationWidth={400}
-        // onChange={this.onChange}
-      />
-
-      <div style={{background: this.state.stringColor, width: 100, height: 50, color: 'white'}}>
-        {JSON.stringify(this.state.color)}
+    return (
+      <div style={{ display: 'flex', flexDirection: 'column' }}>
+        <div style={{ marginBottom: 20 }}>
+          <h3>ColorPicker</h3>
+          <ColorPicker />
+        </div>
+        <div style={{ marginBottom: 40 }}>
+          <h3>ColorPalette</h3>
+          <ColorPalette />
+        </div>
+        <div style={{ marginBottom: 40 }}>
+          <h3>ColorPickerInput</h3>
+          <ColorPickerInput />
+        </div>
+        <div style={{ marginBottom: 20 }}>
+          <h3>ColorTextInput</h3>
+          <ColorTextInput />
+        </div>
       </div>
-    </div>
+    );
   }
-
 }
 
-render(<App />, document.getElementById('content'))
+render(<App />, document.getElementById('content'));
