3b1681005750439fc679a7860fbf6a0ccdd7eea0
zpytchrb <radu.brehar@zippyui.com>
2018-04-05 16:37:11 +0300

    [chore] Cleanup old files and license

--- scripts/cleanGlobalLib.js
+++ scripts/cleanGlobalLib.js
@@ -1,26 +1,19 @@
 /**
- * Copyright 2015-present Zippy Technologies
+ * Copyright (c) 2015-present, Zippy Technologies
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *   http://www.apache.org/licenses/LICENSE-2.0
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
  */
-
-const path = require('path')
-const pify = require('pify')
-const exec = require('child_process').exec
+const path = require('path');
+const pify = require('pify');
+const exec = require('child_process').exec;
 
 function cleanGlobalLib() {
-  const libPath = path.resolve(__dirname, '..', 'lib')
+  const libPath = path.resolve(__dirname, '..', 'lib');
   // remove lib and create lib folder
-  return pify(exec)(`rm -rf ${libPath}`)
-  .then(() => pify(exec)(`mkdir ${libPath}`))
+  return pify(exec)(`rm -rf ${libPath}`).then(() =>
+    pify(exec)(`mkdir ${libPath}`)
+  );
 }
 
-module.exports = cleanGlobalLib
+module.exports = cleanGlobalLib;
