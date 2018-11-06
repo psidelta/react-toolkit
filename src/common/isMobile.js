9e356451a1d33b56fee1cd785af33e7c4be066d1
zpytchrb <radu.brehar@zippyui.com>
2018-11-06 11:08:52 +0200

    patch: Fix isMobile to not break on SSR

--- src/common/isMobile.js
+++ src/common/isMobile.js
@@ -8,7 +8,7 @@
 import hasTouch from '@zippytech/has-touch';
 
 const mobileTest = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
-  navigator.userAgent
+  navigator ? navigator.userAgent : ''
 );
 
 export default hasTouch && mobileTest;
